7e073df12c4a32d8cd75bd5d8bcd90cacf6475ca
Grinseteddy <annegret.kampe@arcor.de>
2019-04-07 20:28:17 +0200

    Initial commit

--- src/main/java/com/annegret/servicepatterns/mapper/MapperController.java
+++ src/main/java/com/annegret/servicepatterns/mapper/MapperController.java
@@ -1,25 +1,103 @@
 package com.annegret.servicepatterns.mapper;
 
+import com.fasterxml.jackson.core.io.JsonEOFException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 @RestController
 
 public class MapperController {
+
     @GetMapping(value = "mapping")
-    public String mapped(@RequestParam("inputString") String inputString) {
-        String returnString = mapp (inputString);
-        return returnString;
+    public String mapped(@RequestParam("inputString") String inputString) throws JSONException {
+
+        try {
+            String returnString = mapp(inputString);
+            return returnString;
+        } catch (JSONException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Master data for input string "+inputString+" not found");
+        }
     }
 
-    private String mapp(String inputString) {
 
-        if (inputString.length()>0) {
+    //TODO Get Mapping information out of configuration
 
-            String mappStringA = inputString.replace('A', 'B');
-            String mappStringB=  mappStringA.replace('a', 'b');
+    private JSONObject Configuration(int configuration) throws JSONException {
+        //TODO Get configuration index out of configuration
 
-            return mappStringB;
+        try {
+            JSONObject mappingCharacter=getCharacterByIndex("http://localhost:8083/Character/", String.valueOf(configuration));
+            return mappingCharacter;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please select correct configuration");
         }
-        return "";
+
+    }
+
+    private JSONObject getCharacterByIndex(String urlAsString, String parameter) throws IOException, JSONException {
+        try {
+            String urlParameter = urlAsString+parameter;
+            URL url=new URL(urlParameter);
+            HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() != 200) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "According masterdata not found");
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+
+            output = br.readLine();
+            //TODO think about timeouts
+            conn.disconnect();
+
+            JSONObject jsonReturn=new JSONObject(output);
+
+            return jsonReturn;
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Master data not available");
+        }
+    }
+
+    private String mapp(String inputString) throws JSONException {
+
+        try {
+            if (inputString.length() > 0) {
+
+                JSONObject toBeMapped = Configuration(0);
+                String toBeMappedCapital= (String)toBeMapped.get("capital");
+                String toBeMappedSmallLetter =(String)toBeMapped.get("smallLetter");
+
+
+                JSONObject newCharacter= Configuration(5);
+                String newCharacterCapital = (String)newCharacter.get("capital");
+                String newCharacterSmallLetter =(String) newCharacter.get("smallLetter");
+
+
+
+
+                String mappStringA = inputString.replace(toBeMappedCapital.charAt(0), newCharacterCapital.charAt(0));
+                String mappStringB = mappStringA.replace(toBeMappedSmallLetter.charAt(0), newCharacterSmallLetter.charAt(0));
+
+                return mappStringB;
+            }
+            return "";
+        } catch (JSONException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Master data answer not well formed");
+        }
     }
 }
